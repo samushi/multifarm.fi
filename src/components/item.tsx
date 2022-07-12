110e9398e229337478df649de6081f4994b84caa
Sami Maxhuni <samimaxhuni@kutia.net>
2022-07-12 20:33:28 +0200

    finished

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -12,9 +12,15 @@ const Item = (): JSX.Element => {
   if (!data) return <div>Loading...</div>;
 
   return (
-    <div className="flex">
-      <ArpChart data={data.aprHistory} />
-      <TvlChart data={data.tvlStakedHistory} />
+    <div className="flex w-full">
+      <div className="w-1/2 bg-gradient-to-r from-[#303659] to-[#292E4D] my-4 mr-2 rounded-lg">
+        <h2 className="text-center mb-5 font-mono my-5">Asset APR(y)</h2>
+        <ArpChart data={data.aprHistory} />
+      </div>
+      <div className="w-1/2 bg-gradient-to-r from-[#303659] to-[#292E4D] my-4 ml-2 rounded-lg">
+        <h2 className="text-center mb-5 font-mono my-5">Asset TVL</h2>
+        <TvlChart data={data.tvlStakedHistory} />
+      </div>
     </div>
   );
 };
